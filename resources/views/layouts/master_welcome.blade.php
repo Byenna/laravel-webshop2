0171b3a924cc45cef00b40198787c560a073c7f5
Juraia Pinas <byenna21@gmail.com>
2022-03-08 13:43:17 +0100

    modified

--- resources/views/layouts/master_welcome.blade.php
+++ resources/views/layouts/master_welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,20 +11,23 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}"></head>
-<body class="welcome_body_img">
-<!-- <div id="app"> -->
-@include('layouts.header_welcome')
-@include('layouts.side_nav')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
 
-@yield('content')
-<!-- </div> -->
+<body class="welcome_body_img">
+    <!-- <div id="app"> -->
+    @include('layouts.header_welcome')
+    @include('layouts.side_nav')
+
+    @yield('content')
+    <!-- </div> -->
 </body>
+
 </html>
