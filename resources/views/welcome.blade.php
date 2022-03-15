bbb3794ca4a42de135d3df21ae3fc409bd997967
Juraia Pinas <byenna21@gmail.com>
2022-03-15 13:46:37 +0100

    welcome page ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.master_head')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+@include('layouts.header_welcome_and_products')
+<div class="welcome_page">
+    <div class="image-space"></div>
+@include('layouts.footer')
+</div>
+@endsection
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-
-    <div id="app" ></div>
-    <div class="welcome_page">
-        <!-- <div class='welcome_page'> -->
-        @include('layouts.header_welcome')
-        
-
-        <div class="image-space"></div>
-    <!-- <img class='img-fluid' src="/images/coffeeGif1.gif" alt="coffee image"> -->
-        @include('layouts.footer')
-        </div>
-    </div>
-
-</html>
 
 
 
