2859b437dcd3377a2121cd8171871ef55d581b04
Juraia Pinas <byenna21@gmail.com>
2022-03-16 10:11:00 +0100

    adjusted register and checkoutpage

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -77,17 +77,6 @@
                 </div>
     
                 <div class="col-12">
-                  <label for="username" class="form-label">Username</label>
-                  <div class="input-group has-validation">
-                    <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" id="username" placeholder="Username" required>
-                  <div class="invalid-feedback">
-                      Your username is required.
-                    </div>
-                  </div>
-                </div>
-    
-                <div class="col-12">
                   <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
                   <input type="email" class="form-control" id="email" placeholder="you@example.com">
                   <div class="invalid-feedback">
@@ -95,17 +84,39 @@
                   </div>
                 </div>
     
-                <div class="col-12">
-                  <label for="address" class="form-label">Address</label>
-                  <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+                <div class="col-sm-12">
+                  <label for="address" class="form-label">Street</label>
+                  <input type="text" class="form-control" id="street" placeholder="Brinkstraat" required>
                   <div class="invalid-feedback">
                     Please enter your shipping address.
                   </div>
                 </div>
     
-                <div class="col-12">
-                  <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                  <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+                <div class="col-sm-4">
+                  <label for="house_number" class="form-label">House Number </label>
+                  <input type="text" class="form-control" id="house_number" placeholder="17">
+                </div>
+
+                <div class="col-md-3">
+                  <label for="zip" class="form-label">Zip</label>
+                  <input type="text" class="form-control" id="zip" placeholder="" required>
+                  <div class="invalid-feedback">
+                    Zip code required.
+                  </div>
+                </div>
+              </div>
+    
+                
+    
+                <div class="col-md-4">
+                  <label for="state" class="form-label">City</label>
+                  <select class="form-select" id="state" required>
+                    <option value="">Choose...</option>
+                    <option>California</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    Please provide a valid state.
+                  </div>
                 </div>
     
                 <div class="col-md-5">
@@ -118,26 +129,6 @@
                     Please select a valid country.
                   </div>
                 </div>
-    
-                <div class="col-md-4">
-                  <label for="state" class="form-label">State</label>
-                  <select class="form-select" id="state" required>
-                    <option value="">Choose...</option>
-                    <option>California</option>
-                  </select>
-                  <div class="invalid-feedback">
-                    Please provide a valid state.
-                  </div>
-                </div>
-    
-                <div class="col-md-3">
-                  <label for="zip" class="form-label">Zip</label>
-                  <input type="text" class="form-control" id="zip" placeholder="" required>
-                  <div class="invalid-feedback">
-                    Zip code required.
-                  </div>
-                </div>
-              </div>
     
               <hr class="my-4">
     
