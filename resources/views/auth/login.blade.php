766a645c11df91d82363186c7a92b1e5e202e72c
Juraia Pinas <byenna21@gmail.com>
2022-03-21 14:28:04 +0100

    styled auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div id="login" class="card">
+            <div class="card auth">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
